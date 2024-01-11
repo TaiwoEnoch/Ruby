2c86ed87467b34043330002478342585f7cf2d70
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-01-11 16:03:42 +0100

    Create hello ruby

---
+++ hello.rb
@@ -0,0 +1,22 @@
+puts "Hello Ruby!"
+
+#!/user/bin/ruby -w
+
+puts ("Unary operator")
+puts(~5)
+puts(~-5)
+puts(!true)
+puts(!false)
+
+puts("Ternary Operators")
+puts(2<5?5:2)
+puts(5<2?5:2)
+
+puts("Comparison operator")
+puts(2 == 5)
+puts(2 != 5)
+puts(2 > 5)
+puts(2 < 5)
+puts(2 >= 5)
+puts(2 <=  5)
+
