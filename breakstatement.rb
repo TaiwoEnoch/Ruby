48784094015d86a9d4f1308889b3e72319dbc3d4
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-01-11 16:07:41 +0100

    Recap basis of Ruby

---
+++ breakstatement.rb
@@ -0,0 +1,18 @@
+# Ruby break statement ------------------>
+
+# i = 1
+# while true
+#   if i * 5 >= 25
+#     break
+#   end
+#   puts i * 5
+#   i += 1
+# end
+
+# Next statement -------------------->
+for i in 5...11
+  if i == 7 then
+    next
+  end
+  puts i
+end
