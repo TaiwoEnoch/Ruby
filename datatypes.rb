fd92ceb713a116a9e6b16e8851f4ea510cd8a66c
TaiwoEnoch <taiwoenoch0@gmail.com>
2024-01-11 16:04:50 +0100

    Prractice ruby data types

---
+++ datatypes.rb
@@ -0,0 +1,20 @@
+# Data types represent a type of data such as text, string, number, etc.
+
+# Number 
+# Strings
+# Symbols
+# Hashes
+# Arrays
+# Booleans
+
+# Hashes --------------------------->
+data = {"Akash" => "Physics","Ankit" => "Chemistry","Aman" => "Maths"}
+puts data["Akash"]
+puts data["Ankit"]
+puts data["Aman"]
+
+#Arrays ------------------------------>
+data = ["Akash", "Ankit", "Aman"]
+puts data[0]
+puts data[1]
+puts data[2]
